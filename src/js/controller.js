ae04d5bb84da53d98613848f90a1a2a571a2077b
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-09-18 09:44:09 +0200

    maj .gitignore & script

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,36 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza&key=56b00414-7c78-4f8d-a174-1d3f182bb92e'
+    );
+    const data = await res.json();
+
+    if(!res.ok) {throw new Error(`${data.message}(${res.status})`);}
+
+      for (let recipe of data.data.recipes){
+      console.log(recipe);
+      }
+
+
+      // recipes = {
+      //   id: recipes.id,
+      //   title: recipes.title,
+      //   publisher: recipes.publisher,
+      //   sourceUrl: recipes.source_url,
+      //   image: recipes.image_url,
+      //   servings: recipes.servings,
+      //   cookingTime: recipes.cooking_time,
+      //   ingredients: recipes.ingredients
+      // }
+      
+    console.log(data);
+
+  } catch (err) {
+    alert(err);
+  }
+};
+showRecipe();
